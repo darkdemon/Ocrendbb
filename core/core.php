26436f6a50d42e4f5f6411cb6942554f620df8ed
darkdemon <darkdemon_9006@hotmail.com>
2016-03-30 00:36:47 -0500

    Login
    En esta oportunidad se hace el login y la estructura de toda la web

---
+++ core/core.php
@@ -0,0 +1,8 @@
+<?php
+/*Este es el nucleo de la aplicacion*/
+define('HTML_DIR', 'html/');
+define('APP_TITLE', 'OcrendBB');
+define('APP_COPY','Copyright &copy'. date('Y', time()).' Ocrend Software');
+
+require('vendor/autoload.php');
+ ?>
